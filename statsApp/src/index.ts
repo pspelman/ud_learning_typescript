39c98461bf409c085dab138a3f411c1a51a98048
Philip Spelman <philip.spelman@vaporstream.com>
2021-02-01 13:44:40 -0500

    - showed how to use a STATIC method to create a preconfigured instance of summary

--- statsApp/src/index.ts
+++ statsApp/src/index.ts
@@ -3,6 +3,10 @@ import {CsvFileReader} from "./CsvFileReader";
 import {MatchReader} from "./MatchReader";
 import {MatchResult} from "./MatchResult";
 import {match} from "assert";
+import {ConsoleReport} from "./reportTargets/ConsoleReport";
+import {WinsAnalysis} from "./WinsAnalysis";
+import {Summary} from "./Summary";
+import {HtmlReport} from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader('football.csv')
 
@@ -14,5 +18,14 @@ matchReader.load()
 
 // console.log(matches)
 const MAN_UNITED = 'Man United'
+// const summary = new Summary(new WinsAnalysis(MAN_UNITED), new ConsoleReport())
+// const summary = new Summary(new WinsAnalysis(MAN_UNITED), new HtmlReport())
+// summary.buildAndPrintReport(matchReader.matches)
 
+const easySummary = Summary.winsAnalysisWithHtmlReport(MAN_UNITED)
+easySummary.buildAndPrintReport(matchReader.matches)
+
+// new analysis
+// new report
+// new summary
 
