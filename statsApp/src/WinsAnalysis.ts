752ef5ceb63d21bd117e699e07afba2327aebd36
Philip Spelman <philip.spelman@vaporstream.com>
2021-02-01 13:29:23 -0500

    - added reportTargets dir   - added ConsoleReport.ts for printing reports to console - updated index.ts - updated Summary.ts with a new buildAndPrintReport method - added WinsAnalysis.ts to analyze the games data

---
+++ statsApp/src/WinsAnalysis.ts
@@ -0,0 +1,22 @@
+import {Analyzer} from "./Summary";
+import {MatchData} from "./MatchData";
+import {MatchResult} from "./MatchResult";
+
+export class WinsAnalysis implements Analyzer {
+	// take in the name of a team and find the number of times the team won
+	constructor(public team: string) {}
+
+	run(matches: MatchData[]): string {
+		let wins = 0
+		for (let match of matches) {
+			// they were they home team, and the home team won or they were the away team and the away team won
+			if ((match[1] === this.team && match[5] === MatchResult.HomeWin) ||
+				(match[2] === this.team && match[5] === MatchResult.AwayWin)) {
+				wins ++
+			}
+		}
+		let results = `Team ${this.team} has ${wins} wins`;
+		console.log(results)
+		return results
+	}
+}
--- statsApp/src/index.ts
+++ statsApp/src/index.ts
@@ -14,22 +14,5 @@ matchReader.load()
 
 // console.log(matches)
 const MAN_UNITED = 'Man United'
-let wins = 0
-let team = MAN_UNITED
-for (let match of matchReader.matches) {
-	// they were they home team, and the home team won or they were the away team and the away team won
-	if ((match[1] === team && match[5] === MatchResult.HomeWin) ||
-		(match[2] === team && match[5] === MatchResult.AwayWin)) {
-		wins ++
-	}
-}
-console.log(`${team} wins: `, wins)
 
-// if (match[1] === MAN_UNITED && match[5] === "H")
-// {
-// 	wins++
-// } else if (match[2] === MAN_UNITED && match[5] === "A")
-// {
-// 	wins++
-// }
 
